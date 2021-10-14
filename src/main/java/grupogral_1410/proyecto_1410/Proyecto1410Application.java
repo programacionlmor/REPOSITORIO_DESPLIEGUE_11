063f10287c89e702867e532165e4fa1b800b25d0
LUZ MARINA ORJUELA <programacion.luz.rojas@gmail.com>
2021-10-14 08:18:15 -0500

    VERSION DESPLIEGUE

---
+++ src/main/java/grupogral_1410/proyecto_1410/Proyecto1410Application.java
@@ -0,0 +1,13 @@
+package grupogral_1410.proyecto_1410;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1410Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1410Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupogral_1410/proyecto_1410/Proyecto1410ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupogral_1410.proyecto_1410;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1410ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
